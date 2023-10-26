ce70994a1c31947c8ccb21c231b37017b740250a
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2023-10-26 10:55:19 +0530

    final

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Dark Mode Toggle</h1>
+          <p className="py-6">
+            This is just a sample project to tutor the theme toggling
+            capabilities.
+          </p>
+          <button className="btn btn-primary">This is just for learning</button>
+        </div>
+      </div>
+    </div>
+  );
+}
